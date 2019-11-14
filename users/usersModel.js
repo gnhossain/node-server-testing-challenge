47659eb4255adc5a52c29cfc2219cdc7f0246bfa
G H <gh@Gs-MacBook-Pro.local>
2019-11-14 15:45:49 -0800

    mvp

---
+++ users/usersModel.js
@@ -0,0 +1,40 @@
+const db = require("../data/dbConfig.js");
+
+module.exports = {
+  insert,
+  update,
+  remove,
+  getAll,
+  findById
+};
+
+function insert(user) {
+  return (
+    db("users")
+      .insert(user, "id")
+      // .then(([id]) => {
+      .then(ids => {
+        const id = ids[0];
+        // const [id] = ids;
+        return db("users")
+          .where({ id })
+          .first();
+      })
+  );
+}
+
+async function update(id, changes) {
+  return null;
+}
+
+function remove(id) {
+  return null;
+}
+
+function getAll() {
+  return db("users");
+}
+
+function findById(id) {
+  return null;
+}
